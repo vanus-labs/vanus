d39dfb662e83d197c6558948160c0874d5068292
wenfeng <sxian.wang@gmail.com>
2022-05-12 11:49:35 +0800

    add gateway_endpoint in Ping

--- internal/controller/config.go
+++ internal/controller/config.go
@@ -33,6 +33,7 @@ type Config struct {
 	EtcdConfig           embedetcd.Config  `yaml:"embed_etcd"`
 	Topology             map[string]string `yaml:"topology"`
 	Replicas             uint              `yaml:"replicas"`
+	GatewayEndpoint      string            `yaml:"gateway_endpoint"`
 }
 
 func (c *Config) GetEtcdConfig() embedetcd.Config {
@@ -49,6 +50,7 @@ func (c *Config) GetEventbusCtrlConfig() eventbus.Config {
 		KVKeyPrefix:      c.MetadataConfig.KeyPrefix,
 		Replicas:         c.Replicas,
 		Topology:         c.Topology,
+		GatewayEndpoint:  c.GatewayEndpoint,
 	}
 }
 
