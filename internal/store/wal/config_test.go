6b4922e15d3c62846fbe68f7ce95f52f6d0ad65c
James Yin <ywhjames@hotmail.com>
2022-06-20 03:05:19 +0000

    test: wal config

---
+++ internal/store/wal/config_test.go
@@ -0,0 +1,55 @@
+// Copyright 2022 Linkall Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package wal
+
+import (
+	// standard libraries.
+	"testing"
+	"time"
+
+	// third-party libraries.
+	. "github.com/smartystreets/goconvey/convey"
+
+	// this project.
+	"github.com/linkall-labs/vanus/internal/store/wal/io"
+)
+
+func TestConfig(t *testing.T) {
+	Convey("wal config", t, func() {
+		engine := io.NewEngine()
+		cfg := makeConfig("wal-test",
+			WithPosition(1024),
+			WithBlockSize(1024),
+			WithFileSize(4*1024*1024),
+			WithFlushTimeout(5*time.Millisecond),
+			WithAppendBufferSize(1),
+			WithCallbackBufferSize(1),
+			WithFlushBufferSize(1),
+			WithWakeupBufferSize(1),
+			WithIOEngine(engine),
+		)
+
+		So(cfg.pos, ShouldEqual, 1024)
+		So(cfg.stream.dir, ShouldEqual, "wal-test")
+		So(cfg.stream.blockSize, ShouldEqual, 1024)
+		So(cfg.stream.fileSize, ShouldEqual, 4*1024*1024)
+		So(cfg.flushTimeout, ShouldEqual, 5*time.Millisecond)
+		So(cfg.appendBufferSize, ShouldEqual, 1)
+		So(cfg.callbackBufferSize, ShouldEqual, 1)
+		So(cfg.flushBufferSize, ShouldEqual, 1)
+		So(cfg.wakeupBufferSize, ShouldEqual, 1)
+		So(cfg.engine, ShouldEqual, engine)
+	})
+}
