410ede28db31277b1110cc390c3d23bbc49260ae
delu <delu.xu@linkall.com>
2022-12-15 11:01:37 +0800

    refactor: change format date function
    Signed-off-by: delu <delu.xu@linkall.com>

--- go.mod
+++ go.mod
@@ -23,6 +23,7 @@ require (
 	github.com/iceber/iouring-go v0.0.0-20220609112130-b1dc8dd9fbfd
 	github.com/jedib0t/go-pretty/v6 v6.3.1
 	github.com/json-iterator/go v1.1.12
+	github.com/labstack/gommon v0.4.0
 	github.com/linkall-labs/embed-etcd v0.1.1
 	github.com/linkall-labs/vanus/client v0.5.1
 	github.com/linkall-labs/vanus/observability v0.5.1
---
+++ internal/primitive/transform/function/util/time.go
@@ -0,0 +1,48 @@
+// Copyright 2022 Linkall Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package util
+
+import "bytes"
+
+var formats = map[byte]string{
+	'd': "02",   // Day:    Day of the month, 2 digits with leading zeros. Eg: 01 to 31.
+	'm': "01",   // Month:  Numeric representation of a month, with leading zeros. Eg: 01 through 12.
+	'Y': "2006", // Year:   A full numeric representation of a year, 4 digits. Eg: 1999 or 2003.
+	'y': "06",   // Year:   A two digit representation of a year. Eg: 99 or 03.
+	'h': "03",   // Time:   12-hour format of an hour with leading zeros. Eg: 01 through 12.
+	'H': "15",   // Time:   24-hour format of an hour with leading zeros. Eg: 00 through 23.
+	'i': "04",   // Time:   Minutes with leading zeros. Eg: 00 to 59.
+	's': "05",   // Time:   Seconds with leading zeros. Eg: 00 through 59.
+}
+
+// ConvertFormat2Go converts format to layout.
+func ConvertFormat2Go(format string) string {
+	buffer := bytes.NewBuffer(nil)
+	for i := 0; i < len(format); i++ {
+		if layout, ok := formats[format[i]]; ok {
+			buffer.WriteString(layout)
+		} else {
+			switch format[i] {
+			case '\\': // raw output, no parse
+				buffer.WriteByte(format[i+1])
+				i++
+				continue
+			default:
+				buffer.WriteByte(format[i])
+			}
+		}
+	}
+	return buffer.String()
+}
