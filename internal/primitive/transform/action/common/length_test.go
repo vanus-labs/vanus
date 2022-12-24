be0087e222f890bfcdfb01d4fb229dcd29a98f11
delu <delu.xu@linkall.com>
2022-12-24 20:01:18 +0800

    feat: add function length (#364)
    Signed-off-by: delu <delu.xu@linkall.com>
    
    Signed-off-by: delu <delu.xu@linkall.com>

---
+++ internal/primitive/transform/action/common/length_test.go
@@ -0,0 +1,110 @@
+// Copyright 2022 Linkall Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package common_test
+
+import (
+	stdJson "encoding/json"
+	"testing"
+
+	cetest "github.com/cloudevents/sdk-go/v2/test"
+	"github.com/linkall-labs/vanus/internal/primitive/transform/action/common"
+	"github.com/linkall-labs/vanus/internal/primitive/transform/context"
+	"github.com/linkall-labs/vanus/internal/primitive/transform/runtime"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestLengthAction(t *testing.T) {
+	funcName := common.NewLengthAction().Name()
+	Convey("test length", t, func() {
+		jsonStr := `{
+			"array": ["123",4,true],
+			"string": "string",
+			"map":{
+				"key1":"value1",
+				"key2":"value2"
+			},
+			"number": 10,
+			"bool": false
+		}`
+		Convey("test array", func() {
+			a, err := runtime.NewAction([]interface{}{funcName, "$.data.length", "$.data.array"})
+			So(err, ShouldBeNil)
+			e := cetest.MinEvent()
+			var data map[string]interface{}
+			err = stdJson.Unmarshal([]byte(jsonStr), &data)
+			So(err, ShouldBeNil)
+			err = a.Execute(&context.EventContext{
+				Event: &e,
+				Data:  data,
+			})
+			So(err, ShouldBeNil)
+			So(data["length"], ShouldEqual, 3)
+		})
+		Convey("test string", func() {
+			a, err := runtime.NewAction([]interface{}{funcName, "$.data.length", "$.data.string"})
+			So(err, ShouldBeNil)
+			e := cetest.MinEvent()
+			var data map[string]interface{}
+			err = stdJson.Unmarshal([]byte(jsonStr), &data)
+			So(err, ShouldBeNil)
+			err = a.Execute(&context.EventContext{
+				Event: &e,
+				Data:  data,
+			})
+			So(err, ShouldBeNil)
+			So(data["length"], ShouldEqual, 6)
+		})
+		Convey("test map", func() {
+			a, err := runtime.NewAction([]interface{}{funcName, "$.data.length", "$.data.map"})
+			So(err, ShouldBeNil)
+			e := cetest.MinEvent()
+			var data map[string]interface{}
+			err = stdJson.Unmarshal([]byte(jsonStr), &data)
+			So(err, ShouldBeNil)
+			err = a.Execute(&context.EventContext{
+				Event: &e,
+				Data:  data,
+			})
+			So(err, ShouldBeNil)
+			So(data["length"], ShouldEqual, 2)
+		})
+		Convey("test bool", func() {
+			a, err := runtime.NewAction([]interface{}{funcName, "$.data.length", "$.data.bool"})
+			So(err, ShouldBeNil)
+			e := cetest.MinEvent()
+			var data map[string]interface{}
+			err = stdJson.Unmarshal([]byte(jsonStr), &data)
+			So(err, ShouldBeNil)
+			err = a.Execute(&context.EventContext{
+				Event: &e,
+				Data:  data,
+			})
+			So(err, ShouldNotBeNil)
+		})
+		Convey("test number", func() {
+			a, err := runtime.NewAction([]interface{}{funcName, "$.data.length", "$.data.number"})
+			So(err, ShouldBeNil)
+			e := cetest.MinEvent()
+			var data map[string]interface{}
+			err = stdJson.Unmarshal([]byte(jsonStr), &data)
+			So(err, ShouldBeNil)
+			err = a.Execute(&context.EventContext{
+				Event: &e,
+				Data:  data,
+			})
+			So(err, ShouldNotBeNil)
+		})
+	})
+}
