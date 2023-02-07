a0040e58bacf440f3d814c082f5f48e8e406f4a7
wenfeng <sxian.wang@gmail.com>
2023-02-07 14:53:40 +0800

    update review commets

--- test/regression/run.sh
+++ test/regression/run.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 set -ex
 
-vsctl eventbus create eventbus --name "${EVENTBUS}" --eventlog "${EVENTLOG_NUMBER}"
-vsctl subscription create --name "${EVENTBUS}" \
-  --eventbus "${EVENTBUS}" \
+vsctl eventbus create eventbus --name "regression_pubsub" --eventlog "4"
+vsctl subscription create --name "regression_pubsub" \
+  --eventbus "regression_pubsub" \
   --sink "grpc://host.docker.internal:18080" \
-  --procotol "grpc"
+  --protocol "grpc"
 
 # start publish and subscribe case
-/vanus/regression/pubsub
+/vanus/test/regression/bin/pubsub
+
+vsctl eventbus create eventbus --name "regression_time" --eventlog "4"
+vsctl subscription create --name "regression_time" \
+  --eventbus "regression_time" \
+  --sink "grpc://host.docker.internal:18081" \
+  --protocol "grpc"
+
+/vanus/test/regression/bin/timer
 
 # vsctl eventbus delete "${EVENTBUS}"
 # vsctl eventbus delete "${NOTIFICATION_EVENTBUS}"
