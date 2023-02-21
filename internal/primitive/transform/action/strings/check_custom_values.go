e8da7878bbbd808853c05863f65205dac84388d6
Dhanu Saputra <35093673+dhanusaputra@users.noreply.github.com>
2023-02-21 15:50:20 +0700

    feat: implement function "check_custom_values" (#440)
    * feat: implement function "check_custom_values"
    
    * rename
    
    * fix lint
    
    * fix comment
    
    * fix review

---
+++ internal/primitive/transform/action/strings/check_custom_values.go
@@ -0,0 +1,68 @@
+// Copyright 2023 Linkall Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package strings
+
+import (
+	"strings"
+
+	"github.com/linkall-labs/vanus/internal/primitive/transform/action"
+	"github.com/linkall-labs/vanus/internal/primitive/transform/arg"
+	"github.com/linkall-labs/vanus/internal/primitive/transform/common"
+	"github.com/linkall-labs/vanus/internal/primitive/transform/context"
+)
+
+type checkCustomValueAction struct {
+	action.CommonAction
+}
+
+// NewCheckCustomValuesAction ["check_custom_values","sourceJsonPath", "customValue",
+// "targetJsonPath", "trueFlagReplacement", "falseFlagReplacement"].
+func NewCheckCustomValuesAction() action.Action {
+	return &checkCustomValueAction{
+		CommonAction: action.CommonAction{
+			ActionName: "CHECK_CUSTOM_VALUES",
+			FixedArgs: []arg.TypeList{
+				arg.EventList,
+				[]arg.Type{arg.Constant},
+				arg.EventList,
+				[]arg.Type{arg.Constant},
+				[]arg.Type{arg.Constant},
+			},
+		},
+	}
+}
+
+func (a *checkCustomValueAction) Init(args []arg.Arg) error {
+	a.TargetArg = args[2]
+	a.Args = args[:2]
+	a.Args = append(a.Args, args[3:]...)
+	a.ArgTypes = []common.Type{common.String, common.String, common.Any, common.Any}
+	return nil
+}
+
+func (a *checkCustomValueAction) Execute(ceCtx *context.EventContext) error {
+	args, err := a.RunArgs(ceCtx)
+	if err != nil {
+		return err
+	}
+	str, _ := args[0].(string)
+	customValue, _ := args[1].(string)
+	trueFlagReplacement := args[2]
+	falseFlagReplacement := args[3]
+	if strings.Contains(str, customValue) {
+		return a.TargetArg.SetValue(ceCtx, trueFlagReplacement)
+	}
+	return a.TargetArg.SetValue(ceCtx, falseFlagReplacement)
+}
