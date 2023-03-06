74985f559de93e95df53b34f4f66da8d7b67e9d0
Jeffrey Whewhetu <106184818+c0d33ngr@users.noreply.github.com>
2023-03-06 13:48:07 +0100

    feat:Function extract missing (#470)
    * Add extract_missing function
    
    * Register extract_missing function in init.go file
    
    * Create test for extract_missing function
    
    * Fix errors in extract_missing function
    
    * Update test for extract_missing function
    
    * Add extract_missing function with bench test
    
    * Resolve extract_missing function to past golangci-lint
    
    * Formatted strings_functions.go file correctly
    
    ---------
    
    Co-authored-by: delu <xdlbdy@gmail.com>

--- internal/primitive/transform/runtime/init.go
+++ internal/primitive/transform/runtime/init.go
@@ -53,6 +53,7 @@ func init() {
 		strings.NewReplaceBetweenPositionsAction,
 		strings.NewCapitalizeSentenceAction,
 		strings.NewCheckCustomValuesAction,
+		strings.NewExtractMissingAction,
 		strings.NewReplaceBetweenDelimitersAction,
 		strings.NewCapitalizeWordAction,
 		strings.NewSplitWithDelimiterAction,
