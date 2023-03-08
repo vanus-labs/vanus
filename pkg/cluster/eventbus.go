ce5740b6a927d73ba4a348fc8fd6ec9b7784f427
wenfeng <sxian.wang@gmail.com>
2023-03-08 21:20:03 +0800

    updae

--- pkg/cluster/eventbus.go
+++ pkg/cluster/eventbus.go
@@ -2,10 +2,9 @@ package cluster
 
 import (
 	"context"
-	"fmt"
-	"strings"
-
 	ctrlpb "github.com/vanus-labs/vanus/proto/pkg/controller"
+	"github.com/vanus-labs/vanus/proto/pkg/meta"
+	"google.golang.org/protobuf/types/known/wrapperspb"
 
 	"github.com/vanus-labs/vanus/pkg/cluster/raw_client"
 )
@@ -19,38 +18,48 @@ type eventbusService struct {
 	client ctrlpb.EventbusControllerClient
 }
 
-func newEventbusService(cc *raw_client.Conn) EventbusService {
-	return &eventbusService{client: raw_client.NewEventbusClient(cc)}
+func (es *eventbusService) GetSystemEventbusByName(ctx context.Context, name string) (*meta.Eventbus, error) {
+	return es.client.GetEventbusWithHumanFriendly(ctx, &ctrlpb.GetEventbusWithHumanFriendlyRequest{
+		Namespace:    "system",
+		EventbusName: name,
+	})
 }
 
-func (es *eventbusService) IsExist(ctx context.Context, name string) bool {
-	_, err := es.client.GetEventbus(ctx, &ctrlpb.GetEventbusRequest{
-		Name: name,
+func (es *eventbusService) GetEventbus(ctx context.Context, id uint64) (*meta.Eventbus, error) {
+	return es.client.GetEventbus(ctx, wrapperspb.UInt64(id))
+}
+
+func (es *eventbusService) IsSystemEventbusExistByName(ctx context.Context, name string) bool {
+	_, err := es.client.GetEventbusWithHumanFriendly(ctx, &ctrlpb.GetEventbusWithHumanFriendlyRequest{
+		Namespace:    "system",
+		EventbusName: name,
 	})
 	return err == nil
 }
 
-func (es *eventbusService) CreateSystemEventbusIfNotExist(ctx context.Context, name string, desc string) error {
-	if es.IsExist(ctx, name) {
-		return nil
+func newEventbusService(cc *raw_client.Conn) EventbusService {
+	return &eventbusService{client: raw_client.NewEventbusClient(cc)}
+}
+
+func (es *eventbusService) IsExist(ctx context.Context, id uint64) bool {
+	_, err := es.client.GetEventbus(ctx, wrapperspb.UInt64(id))
+	return err == nil
+}
+
+func (es *eventbusService) CreateSystemEventbusIfNotExist(ctx context.Context, name string, desc string) (*meta.Eventbus, error) {
+	if es.IsSystemEventbusExistByName(ctx, name) {
+		return nil, nil
 	}
 
-	_, err := es.client.CreateSystemEventbus(ctx, &ctrlpb.CreateEventbusRequest{
+	return es.client.CreateSystemEventbus(ctx, &ctrlpb.CreateEventbusRequest{
 		Name:        name,
 		LogNumber:   int32(defaultSystemEventbusEventlog),
 		Description: desc,
 	})
-	return err
 }
 
-func (es *eventbusService) Delete(ctx context.Context, name string) error {
-	if !strings.HasPrefix(name, systemEventbusPrefix) {
-		return fmt.Errorf("the system eventbus must start with %s", systemEventbusPrefix)
-	}
-
-	_, err := es.client.DeleteEventbus(ctx, &ctrlpb.DeleteEventbusRequest{
-		Name: name,
-	})
+func (es *eventbusService) Delete(ctx context.Context, id uint64) error {
+	_, err := es.client.DeleteEventbus(ctx, wrapperspb.UInt64(id))
 	return err
 }
 
