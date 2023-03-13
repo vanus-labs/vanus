0e29cea18bf71138a688902c0997752cbbacc13c
wenfeng <sxian.wang@gmail.com>
2023-03-13 14:57:01 +0800

    build: update all in one docker

--- build/all-in-one/run.sh
+++ build/all-in-one/run.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+nohup /vanus/etcd/etcd > /vanus/logs/etcd.log &
 nohup /vanus/bin/controller --config /vanus/config/controller.yaml > /vanus/logs/controller.log &
 nohup /vanus/bin/store --config /vanus/config/store.yaml > /vanus/logs/store.log &
 nohup /vanus/bin/trigger --config /vanus/config/trigger.yaml > /vanus/logs/trigger.log &
 nohup /vanus/bin/timer --config /vanus/config/timer.yaml > /vanus/logs/timer.log &
 nohup /vanus/bin/gateway --config /vanus/config/gateway.yaml > /vanus/logs/gateway.log &
 
-sleep 3600
+sleep 999999
 
 pkill -f vanus
