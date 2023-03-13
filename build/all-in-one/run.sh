92b5ce82deeb542bacbc7a579a2eedb1b31e3f93
wenfeng <sxian.wang@gmail.com>
2023-03-13 15:48:52 +0800

    fix

--- build/all-in-one/run.sh
+++ build/all-in-one/run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-nohup /vanus/etcd/etcd > /vanus/logs/etcd.log &
-nohup /vanus/bin/controller --config /vanus/config/controller.yaml > /vanus/logs/controller.log &
-nohup /vanus/bin/store --config /vanus/config/store.yaml > /vanus/logs/store.log &
-nohup /vanus/bin/trigger --config /vanus/config/trigger.yaml > /vanus/logs/trigger.log &
-nohup /vanus/bin/timer --config /vanus/config/timer.yaml > /vanus/logs/timer.log &
-nohup /vanus/bin/gateway --config /vanus/config/gateway.yaml > /vanus/logs/gateway.log &
+nohup /vanus/etcd/etcd > /vanus/logs/etcd.log 2>&1 &
+nohup /vanus/bin/controller --config /vanus/config/controller.yaml > /vanus/logs/controller.log 2>&1 &
+nohup /vanus/bin/store --config /vanus/config/store.yaml > /vanus/logs/store.log 2>&1 &
+nohup /vanus/bin/trigger --config /vanus/config/trigger.yaml > /vanus/logs/trigger.log 2>&1 &
+nohup /vanus/bin/timer --config /vanus/config/timer.yaml > /vanus/logs/timer.log 2>&1 &
+nohup /vanus/bin/gateway --config /vanus/config/gateway.yaml > /vanus/logs/gateway.log 2>&1 &
 
 sleep 999999
 
